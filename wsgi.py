ddcaaf233bb9c22fdc357d2ca847284ac71fd9fb
Mario Alonso <cutie-unquiet0j@icloud.com>
2025-03-27 19:43:09 -0400

    Added init_db() call to wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,7 @@ from init_db import init_db
 # Initialize database
 with app.app_context():
     db.create_all()
+    init_db()
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
