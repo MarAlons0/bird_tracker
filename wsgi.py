2893184bec5130e556bba1f9843eada10581e511
Mario Alonso <cutie-unquiet0j@icloud.com>
2025-03-27 19:35:45 -0400

    Updated wsgi.py to include database initialization

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
 import os
-from app import app
+from app import app, db
+from init_db import init_db
+
+# Initialize database
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
