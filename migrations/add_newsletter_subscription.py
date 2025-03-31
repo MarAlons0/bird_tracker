95a7cb79e6882fa32497d90339b66f6dfc73ab72
Mario Alonso <cutie-unquiet0j@icloud.com>
2025-03-31 12:42:03 -0400

    Update migration script with correct SQLAlchemy syntax

--- migrations/add_newsletter_subscription.py
+++ migrations/add_newsletter_subscription.py
@@ -1,17 +1,15 @@
-from flask_migrate import Migrate
 from app import create_app, db
-from models import User
 
 def upgrade():
     app = create_app()
     with app.app_context():
-        # Add newsletter_subscription column
-        db.engine.execute('ALTER TABLE users ADD COLUMN newsletter_subscription BOOLEAN DEFAULT TRUE')
-        db.session.commit()
+        with db.engine.connect() as conn:
+            conn.execute('ALTER TABLE users ADD COLUMN newsletter_subscription BOOLEAN DEFAULT TRUE')
+            conn.commit()
 
 def downgrade():
     app = create_app()
     with app.app_context():
-        # Remove newsletter_subscription column
-        db.engine.execute('ALTER TABLE users DROP COLUMN newsletter_subscription')
-        db.session.commit() 
+        with db.engine.connect() as conn:
+            conn.execute('ALTER TABLE users DROP COLUMN newsletter_subscription')
+            conn.commit() 
