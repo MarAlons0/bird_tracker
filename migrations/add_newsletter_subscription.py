a4a8bd570555b27498b0040c37fbc62595801d31
Mario Alonso <cutie-unquiet0j@icloud.com>
2025-03-31 12:43:28 -0400

    Update migration script to use SQLAlchemy text() function

--- migrations/add_newsletter_subscription.py
+++ migrations/add_newsletter_subscription.py
@@ -1,15 +1,16 @@
 from app import create_app, db
+from sqlalchemy import text
 
 def upgrade():
     app = create_app()
     with app.app_context():
         with db.engine.connect() as conn:
-            conn.execute('ALTER TABLE users ADD COLUMN newsletter_subscription BOOLEAN DEFAULT TRUE')
+            conn.execute(text('ALTER TABLE users ADD COLUMN newsletter_subscription BOOLEAN DEFAULT TRUE'))
             conn.commit()
 
 def downgrade():
     app = create_app()
     with app.app_context():
         with db.engine.connect() as conn:
-            conn.execute('ALTER TABLE users DROP COLUMN newsletter_subscription')
+            conn.execute(text('ALTER TABLE users DROP COLUMN newsletter_subscription'))
             conn.commit() 
