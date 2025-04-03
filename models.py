e5b64cd09f3ded10ef4a17136b488166df165c8b
Mario Alonso <cutie-unquiet0j@icloud.com>
2025-04-03 15:49:08 -0400

    Fix CarouselImage model to match database schema

--- models.py
+++ models.py
@@ -86,15 +86,16 @@ class RegistrationRequest(db.Model):
         return f'<RegistrationRequest {self.username}>'
 
 class CarouselImage(db.Model):
-    __tablename__ = 'carousel_image'
+    __tablename__ = 'carousel_images'
     
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(255))
-    filepath = db.Column(db.String(255))
+    filename = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(255))
+    description = db.Column(db.Text)
     order = db.Column(db.Integer, default=0)
     is_active = db.Column(db.Boolean, default=True)
-    upload_date = db.Column(db.DateTime, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __repr__(self):
         return f'<CarouselImage {self.filename}>'
