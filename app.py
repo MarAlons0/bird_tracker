868d553bee7d0dcfcb71f12a90d11519dafa3106
Mario Alonso <cutie-unquiet0j@icloud.com>
2025-04-04 10:10:53 -0400

    Remove clear_mappers call from create_app

--- app.py
+++ app.py
@@ -49,9 +49,6 @@ def create_app():
         app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'].replace('postgres://', 'postgresql://', 1)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Clear SQLAlchemy mapper
-    clear_mappers()
-    
     # Configure session settings for Safari
     app.config['SESSION_COOKIE_SECURE'] = os.getenv('FLASK_ENV') == 'production'
     app.config['SESSION_COOKIE_HTTPONLY'] = True
