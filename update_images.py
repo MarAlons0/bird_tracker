6a9f9249d80016017b6ec179e7911f593d401488
Mario Alonso <cutie-unquiet0j@icloud.com>
2025-04-22 11:30:58 -0400

    Update carousel images with correct Cloudinary URLs and titles

--- update_images.py
+++ update_images.py
@@ -15,14 +15,44 @@ def update_carousel_images():
             images = CarouselImage.query.all()
             logger.info(f"Found {len(images)} carousel images")
             
-            # Update URLs
-            for img in images:
-                if img.filename == "welcome.jpg":
-                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1713750000/bird_tracker/welcome.jpg"
-                elif img.filename == "explore.jpg":
-                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1713750000/bird_tracker/explore.jpg"
-                elif img.filename == "share.jpg":
-                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1713750000/bird_tracker/share.jpg"
+            # Update URLs with the correct images
+            for i, img in enumerate(images):
+                if i == 0:
+                    img.filename = "Wood_duck_20250405_024922.jpg"
+                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1743821362/carousel/Wood_duck_20250405_024922.jpg"
+                    img.title = "Wood Duck"
+                elif i == 1:
+                    img.filename = "Wild_turkey_20250405_024827.jpg"
+                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1743821307/carousel/Wild_turkey_20250405_024827.jpg"
+                    img.title = "Wild Turkey"
+                elif i == 2:
+                    img.filename = "Turkey_vulture_20250405_024641.jpg"
+                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1743821201/carousel/Turkey_vulture_20250405_024641.jpg"
+                    img.title = "Turkey Vulture"
+                elif i == 3:
+                    img.filename = "Tree_swallow_20250405_024527.jpg"
+                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1743821127/carousel/Tree_swallow_20250405_024527.jpg"
+                    img.title = "Tree Swallow"
+                elif i == 4:
+                    img.filename = "Wood_duck_20250405_024922.jpg"
+                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1743821362/carousel/Wood_duck_20250405_024922.jpg"
+                    img.title = "Wood Duck"
+                elif i == 5:
+                    img.filename = "Wild_turkey_20250405_024827.jpg"
+                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1743821307/carousel/Wild_turkey_20250405_024827.jpg"
+                    img.title = "Wild Turkey"
+                elif i == 6:
+                    img.filename = "Turkey_vulture_20250405_024641.jpg"
+                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1743821201/carousel/Turkey_vulture_20250405_024641.jpg"
+                    img.title = "Turkey Vulture"
+                elif i == 7:
+                    img.filename = "Tree_swallow_20250405_024527.jpg"
+                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1743821127/carousel/Tree_swallow_20250405_024527.jpg"
+                    img.title = "Tree Swallow"
+                elif i == 8:
+                    img.filename = "Wood_duck_20250405_024922.jpg"
+                    img.cloudinary_url = "https://res.cloudinary.com/dov36rgse/image/upload/v1743821362/carousel/Wood_duck_20250405_024922.jpg"
+                    img.title = "Wood Duck"
                 
                 logger.info(f"Updated image {img.id}: {img.cloudinary_url}")
             
